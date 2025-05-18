6340e0361708321d6b55ee1f435e525dd721d199
djcc2001 <83192564+djcc2001@users.noreply.github.com>
2025-05-17 21:04:38 -0500

    Codigo para la conexion.py

---
+++ Data/conexion.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+def conectar_sql_server():
+    try:
+        conexion = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};'
+            'SERVER=DESKTOP-ABCAQRM;'       # Servidor
+            'DATABASE=DBPortafolioCursos;' # nombre de la BD
+            'Trusted_Connection=yes;'
+        )
+        print("Conexión exitosa a la base de datos.")
+        return conexion
+    except pyodbc.Error as error:
+        print("Error al conectar a la base de datos:", error)
+        return None
+    
