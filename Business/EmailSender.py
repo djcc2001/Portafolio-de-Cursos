35b010c664c12a749850fc855353f5a08721894c
brayan-221986 <221986@unsaac.edu.pe>
2025-06-13 23:43:24 -0500

    Update EmailSender.py
    Se esta agregando un correo y su contraseña de app para enviar los codigos de verificacion para recuperar contraseñas.

--- Business/EmailSender.py
+++ Business/EmailSender.py
@@ -2,8 +2,8 @@ import smtplib
 from email.mime.text import MIMEText
 
 def enviar_codigo_email(destinatario, codigo):
-    remitente = 'correo-remitente@gmail.com'
-    contraseña = 'clave-app'  # Usa clave de app si es Gmail
+    remitente = 'portafolio.cursos.info@gmail.com'
+    contraseña = 'zfsd sgax mwdt gzhb'  # Usa clave de app si es Gmail
     mensaje = MIMEText(f"Tu código de verificación es: {codigo}")
     mensaje['Subject'] = "Código de recuperación"
     mensaje['From'] = remitente
