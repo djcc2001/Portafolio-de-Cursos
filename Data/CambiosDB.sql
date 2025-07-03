479dbc0e04170b9bdf790f0577cce7acaefff4f7
djcc2001 <denisjair2001@gmail.com>
2025-07-03 11:05:43 -0500

    Arreglos.

--- Data/CambiosDB.sql
+++ Data/CambiosDB.sql
@@ -8,3 +8,16 @@ CREATE TABLE ObservacionMaterial (
     FOREIGN KEY (IdMaterial) REFERENCES MaterialEnseñanza(IdMaterial),
     FOREIGN KEY (IdEvaluador) REFERENCES Usuario(IdUsuario)
 );
+
+-- Tabal Silabo arreglos
+DROP TABLE Silabo;
+
+CREATE TABLE Silabo (
+    IdSilabo INT IDENTITY(1,1) PRIMARY KEY,
+    IdPortafolio INT,
+    TipoSilabo VARCHAR(50),
+    NombreArchivo VARCHAR(200),
+    RutaArchivo VARCHAR(300),
+    FechaSubida DATE,
+    FOREIGN KEY (IdPortafolio) REFERENCES Portafolio(IdPortafolio)
+);
